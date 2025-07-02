2518937b69ff61596025013427af681c55ac673b
loniejay <loniejaydalaygon@gmail.com>
2025-07-02 20:02:17 +0800

    reverted last changes.

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,13 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/{id}/delete', [AddonController::class, 'deleteAddon']);
     });
 
+    //Unavailable Dates
+    Route::prefix('/unavailable-dates')->group(function () {
+        Route::get('', [DateController::class, 'getUnavailableDate']);
+        Route::post('/mark', [DateController::class, 'markUnavailableDate']);
+        Route::post('/{id}/unmark', [DateController::class, 'unmarkUnavailableDate']);
+    });
+
     //Bookings
     Route::prefix('/bookings')->group(function () {
         Route::get('/', [BookingController::class, 'getBookings']);
