4bbb30d0dbee850776314d7824caa9970860212a
loniejay <loniejaydalaygon@gmail.com>
2025-07-14 23:03:41 +0800

    removed space in customer_name in report booking resource.

--- app/Http/Resources/ReportBookingResource.php
+++ app/Http/Resources/ReportBookingResource.php
@@ -19,7 +19,7 @@ class ReportBookingResource extends JsonResource
             'id' => $this->id,
             'booking_date' => Carbon::parse($this->booking_date)->format('F d, Y'),
             'event_name' => $this->event_name,
-            'customer_name ' => $this->customer->full_name,
+            'customer_name' => $this->customer->full_name,
             'total_amount' => $this->billing->total_amount,
         ];
     }   
