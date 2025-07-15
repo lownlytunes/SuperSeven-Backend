717b3ebf19338fa60c43635ce2e1b6acd6ad303e
loniejay <loniejaydalaygon@gmail.com>
2025-07-15 11:18:23 +0800

    updated complete_date in workloadResource.

--- app/Http/Resources/WorkloadResource.php
+++ app/Http/Resources/WorkloadResource.php
@@ -26,8 +26,8 @@ class WorkloadResource extends JsonResource
             'booking_address' => $this->booking_address,
             'package_name' => $this->package->package_name ?? null,
             'ceremony_time' => Carbon::parse($this->ceremony_time)->format('h:i A'),
-            'expected_completion_date' => Carbon::parse($this->completion_date)->addMonths(3)->format('F d, Y'),
-            'completion_date' => Carbon::parse($this->completion_date)->format('F d, Y'),
+            'completion_date' => $this->completion_date ? Carbon::parse($this->completion_date)->format('F d, Y') 
+                    : Carbon::parse($this->booking_date)->addMonths(2)->format('F d, Y'),
             'booking_workload_status' => Booking::DELIVERABLE_STATUS[$this->deliverable_status],
             $this->mergeWhen($request->route()->named('workload.detail', 'workload.update'), [
                 'employee_workload_status' => Booking::WORKLOAD_STATUS[$this->employees()->wherePivot('user_id', $user)->pluck('workload_status')->first()] ?? null
