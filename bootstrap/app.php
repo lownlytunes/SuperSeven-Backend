6fcca925289403c7b87f8e6f585494181b9bb803
loniejay <loniejaydalaygon@gmail.com>
2025-07-20 00:37:53 +0800

    revert from previous changes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
         $middleware->append(StartSession::class);
-        $middleware->validateCsrfTokens();
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
