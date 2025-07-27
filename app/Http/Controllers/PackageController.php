fa3701754be6b2197f9a5708410aba1835e86ec9
loniejay <loniejaydalaygon@gmail.com>
2025-07-28 07:04:00 +0800

    add storage link.

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -11,6 +11,7 @@ use App\Models\Package;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class PackageController extends BaseController
 {
