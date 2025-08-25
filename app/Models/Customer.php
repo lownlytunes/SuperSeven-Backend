765d22bd7d04148d9fb56ec05ebb0248babb9160
loniejay <loniejaydalaygon@gmail.com>
2025-08-25 10:04:49 +0800

    renamed customers table to clients table.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -11,6 +11,13 @@ class Customer extends Model
     use HasFactory;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'clients';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
